ea6f22246810261c26a180c2fe46aaa73df6de52
AleixAznarMasagué <aleix.aznar@enti.cat>
2025-02-19 19:38:45 +0100

    GameManager + Boton + Escena de testeo

---
+++ Assets/Projects/2025/DAM_AJEI/G_3/Scripts/Managers/GameManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EntilandVR.DosCinco.DAM_AJEI.G_TRES
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance;
+
+        [Header("General Stats")]
+        public float generalTimer = 0;
+        public bool matchStarted = false;
+        public int completedDishes = 0;
+        [SerializeField]private float _maxTimer;
+
+        private void Awake()
+        { 
+            if (instance == null)
+            { 
+                instance = this;
+            } 
+            else
+            { 
+                Destroy(this.gameObject);
+                Destroy(this);
+            }
+        }
+        void Start()
+        {
+            generalTimer = _maxTimer;
+        }
+
+        void Update()
+        {
+            if (matchStarted)
+            {
+                generalTimer -= Time.deltaTime;
+                if(generalTimer <= 0)
+                {
+                    generalTimer = 0;
+
+                    //TODO("Trigger Event Match Finished")
+
+                    matchStarted = false;
+                }
+            }
+        }
+
+        public void DishCompleted()
+        {
+            completedDishes++;
+            if (completedDishes == 3)
+            {
+                StartCoroutine(StartMatch());
+            }
+        }
+
+        private IEnumerator StartMatch()
+        {
+            //enseñar texto numero
+            //3
+            yield return new WaitForSeconds(1f);
+            //2
+            yield return new WaitForSeconds(1f);
+            //1
+            yield return new WaitForSeconds(1f);
+            //que ponga que esta abierto el local
+            matchStarted = true;
+        }
+
+    }
+}
