a3d19c62703725a3809e2493ed843d0dcfa5d76f
marc ruiz garcia <marc.ruiz.garcia@enti.com>
2025-02-26 17:54:42 +0100

    pedidos tablet

---
+++ Assets/Projects/2025/DAM_AJEI/G_3/Scripts/Tablet/PedidoTablet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EntilandVR.DosCinco.DAM_AJEI.G_TRES
+{
+    public class Pedido : MonoBehaviour
+    {
+        public Ingredient pedido;
+        [SerializeField] private TimeBar _tBar;
+        [SerializeField] private Image _sprite;
+
+        private void Start()
+        {
+
+        }
+    }
+}
