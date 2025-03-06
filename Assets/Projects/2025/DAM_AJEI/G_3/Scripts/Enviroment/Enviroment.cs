158a2eec53d090bf76ab3076c0411b888fea8e5e
ikerlamasENTI <iker.lamas@enti.cat>
2025-03-06 17:13:37 +0100

    NameSpace de Enviroment

--- Assets/Projects/2025/DAM_AJEI/G_3/Scripts/Enviroment/Enviroment.cs
+++ Assets/Projects/2025/DAM_AJEI/G_3/Scripts/Enviroment/Enviroment.cs
@@ -2,86 +2,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enviroment : MonoBehaviour
+namespace EntilandVR.DosCinco.DAM_AJEI.G_TRES
 {
-    public float speed;
-    public float Realspeed;
-    public bool direction;
-    public bool stop;
-
-    public Transform place1;
-    public Transform place2;
-    public int rand;
-
-    private void Start()
+    public class Enviroment : MonoBehaviour
     {
-        Realspeed = speed;
-    }
+        public float speed;
+        public float Realspeed;
+        public bool direction;
+        public bool stop;
 
-    void Update()
-    {
-        if (direction)
+        public Transform place1;
+        public Transform place2;
+        public int rand;
+
+        private void Start()
         {
-            transform.position += new Vector3(1, 0, 0) * Time.deltaTime * Realspeed;
-        }
-        else
-        {
-            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * Realspeed;
+            Realspeed = speed;
         }
 
-        if(stop)
+        void Update()
         {
-            Realspeed = 0;
-        }
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("limit"))
-        {
-            StartCoroutine(Persons());
-        }
-    }
-
-    IEnumerator Persons()
-    {
-        stop = true;
-        yield return new WaitForSeconds(Random.Range(1, 5));
-
-        rand = Random.Range(0, 2);
-        if(rand == 1)
-        {
-            if (direction == true)
+            if (direction)
             {
-                direction = false;
-                stop = false;
-                Realspeed = speed;
+                transform.position += new Vector3(1, 0, 0) * Time.deltaTime * Realspeed;
             }
             else
             {
-                direction = true;
-                stop = false;
-                Realspeed = speed;
+                transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * Realspeed;
             }
-        }
-        if(rand == 0)
-        {
-            if (direction == true)
+
+            if (stop)
             {
-                transform.position = place1.position;
-                stop = false;
-                Realspeed = speed;               
+                Realspeed = 0;
             }
-            else
-            {
-                transform.position = place2.position;
-                stop = false;
-                Realspeed = speed;
-            }
-            
+
         }
 
-        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("limit"))
+            {
+                StartCoroutine(Persons());
+            }
+        }
+
+        private IEnumerator Persons()
+        {
+            stop = true;
+            yield return new WaitForSeconds(Random.Range(1, 5));
+
+            rand = Random.Range(0, 2);
+            if (rand == 1)
+            {
+                if (direction == true)
+                {
+                    direction = false;
+                    stop = false;
+                    Realspeed = speed;
+                }
+                else
+                {
+                    direction = true;
+                    stop = false;
+                    Realspeed = speed;
+                }
+            }
+            if (rand == 0)
+            {
+                if (direction == true)
+                {
+                    transform.position = place1.position;
+                    stop = false;
+                    Realspeed = speed;
+                }
+                else
+                {
+                    transform.position = place2.position;
+                    stop = false;
+                    Realspeed = speed;
+                }
+
+            }
+
+
+        }
     }
 }
+
